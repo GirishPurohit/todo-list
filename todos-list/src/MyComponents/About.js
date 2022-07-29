98e6977beec75425af43fe259dd5a3cd5d5b8dac
GirishPurohit <girish2001udr@gmail.com>
2022-07-29 15:05:05 +0530

    a todo list created using basics of react
    a  basic todo list using react.

---
+++ todos-list/src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus vero ratione atque nihil debitis, pariatur iusto ducimus tenetur voluptatem laboriosam suscipit delectus voluptatibus repellendus? Natus, laborum consectetur. Nostrum, explicabo velit!</p>
+    </div>
+  )
+}
+
+export default About
